965be1f0071601ed28ba3a2624e87b7932cc5840
QuentinGra <pilou4260042@gmail.com>
2024-05-06 12:32:46 +0200

    feat(user): add update page for users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -13,29 +13,30 @@ class AppFixtures extends Fixture
         private UserPasswordHasherInterface $hasher
     ) {
     }
+
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
 
-        $user = (new User)
+        $user = (new User())
             ->setFirstName('Quentin')
             ->setLastName('Grange')
             ->setEmail('admin@test.com')
             ->setPassword(
-                $this->hasher->hashPassword(new User, 'Test1234!')
+                $this->hasher->hashPassword(new User(), 'Test1234!')
             )
             ->setRoles(['ROLE_ADMIN']);
 
         $manager->persist($user);
 
-        for ($i = 0; $i < 10; $i++) {
-            $user = (new User)
+        for ($i = 0; $i < 10; ++$i) {
+            $user = (new User())
                 ->setFirstName("User $i")
                 ->setLastName('Test')
                 ->setEmail("user-$i@test.com")
                 ->setPassword(
-                    $this->hasher->hashPassword(new User, 'Test1234!')
+                    $this->hasher->hashPassword(new User(), 'Test1234!')
                 );
 
             $manager->persist($user);
