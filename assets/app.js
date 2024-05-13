88a49abcf8c65cae916a967438ef9ef54b0ee2c1
QuentinGra <pilou4260042@gmail.com>
2024-05-13 14:40:36 +0200

    feat(categories): add ajax change visibility for admin articles pages

--- assets/app.js
+++ assets/app.js
@@ -1,12 +1,6 @@
 // import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
 import "./styles/app.scss";
-
 import './js/switchArticle';
+import './js/switchCategorie';
 
 require('bootstrap');
